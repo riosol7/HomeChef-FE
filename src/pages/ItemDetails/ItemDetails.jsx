93fb431a7616b76864340e813a9ace4f4958f81b
piraiojeda <piraiojeda@gmail.com>
2022-01-13 00:41:39 -0800

    tweak

--- src/pages/ItemDetails/ItemDetails.jsx
+++ src/pages/ItemDetails/ItemDetails.jsx
@@ -236,6 +236,7 @@ export default function ItemDetails (props) {
                                 background:'white',
                                 color:'black',
                                 borderRadius:'1rem',
+                                border:'solid 1px #E0BFB8',
                                 width:'15rem',
                                 boxShadow: "0px 5px 5px whitesmoke" ||'0 2.8px 2.2px rgba(0, 0, 0, 0.034),0 6.7px 5.3px rgba(0, 0, 0, 0.048),0 12.5px 10px rgba(0, 0, 0, 0.06), 0 22.3px 17.9px rgba(0, 0, 0, 0.072),0 41.8px 33.4px rgba(0, 0, 0, 0.086),0 100px 80px rgba(0, 0, 0, 0.12)',
                             }}
@@ -245,7 +246,7 @@ export default function ItemDetails (props) {
                                     className='display-1 pb-2'
                                     style={{
                                         fontSize:'4rem',
-                                        // color:'#f53783',
+                                        color:'#B76E79',
                                         margin:'0',
                                     }}
                                     >
